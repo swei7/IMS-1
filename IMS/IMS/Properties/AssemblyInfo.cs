10489e26dfdf114b1e5596165f6cd6573919ac02
Deyi Kong <deyikong@yahoo.com>
2017-12-31 01:24:47 -0600

    initialize project

--- IMS/IMS/Properties/AssemblyInfo.cs
+++ IMS/IMS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e287189-ded0-421e-a358-f57ff9d6719d")]
+[assembly: Guid("8d99e4f3-2843-4409-a996-b2fcb3216e6c")]
 
 // Version information for an assembly consists of the following four values:
 //
