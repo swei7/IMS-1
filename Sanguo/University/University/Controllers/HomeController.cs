c7f68c22e19490a89a0aaf7d5645d1434fc3691d
swei7 <shuwu_wei@outlook.com>
2018-01-13 17:07:41 -0600

    entity framework

---
+++ Sanguo/University/University/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace University.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            SchoolContext schoolContext = new SchoolContext();
+
+            Student zhangSan = new Student { FirstMidName = "San", LastName = "Zhang", EnrollmentDate = DateTime.Now.Date };
+
+            schoolContext.Students.Add(zhangSan);
+            schoolContext.SaveChanges();
+
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page (xhbdsn).";
+
+            
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
